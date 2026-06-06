51b37dba505933d288bb011250d93b8a6d749dca
rishima-17 <rishimaiyappan1529@gmail.com>
2026-06-06 15:28:11 +0530

    CSS - Flexbox & Grid

---
+++ function.js
@@ -0,0 +1,12 @@
+function twoSum(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const comp = target - nums[i];
+        if (map.has(comp)) {
+            return [map.get(comp), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+}
+console.log(twoSum([2, 7, 11, 15], 9));
